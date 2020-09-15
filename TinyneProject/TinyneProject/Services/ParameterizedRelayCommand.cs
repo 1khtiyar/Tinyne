fcf5192227cbc6e15de36711a2e670c4bb04ccd3
1khtiyar <ixty.now@bk.ru>
2020-09-15 19:11:55 +0400

    Interface concept added. Project architecture updated. Commands implemented.

---
+++ TinyneProject/TinyneProject/Services/ParameterizedRelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TinyneProject.Services
+{
+    internal class ParameterizedRelayCommand:ICommand
+    {
+        public Action<object> action;
+
+        public ParameterizedRelayCommand(Action<object> action)
+        {
+            this.action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action.Invoke(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ TinyneProject/TinyneProject/Services/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TinyneProject.Services
+{
+    internal class RelayCommand : ICommand
+    {
+        public Action action;
+
+        public RelayCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action.Invoke();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
