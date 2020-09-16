7d3c0999c82063b70e6798d657184d4da17f30a9
1khtiyar <ixty.now@bk.ru>
2020-09-16 17:27:24 +0400

    Window template styling. - Window manipulation buttons style; ListBox template styling. - Item container style; - ScrollViewer style; Main colors(brushes) added.

--- TinyneProject/TinyneProject/Models/StorageData.cs
+++ TinyneProject/TinyneProject/Models/StorageData.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TinyneProject.Models
 {
-    internal class StorageData
+    internal class StorageData:OnPropertyChangedClass
     {
+
+        private ObservableCollection<Note> notes;
+        public ObservableCollection<Note> Notes
+        {
+            get => notes;
+            set 
+            { 
+                notes = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public StorageData()
+        {
+            Notes = new ObservableCollection<Note>();
+        }
     }
 }
