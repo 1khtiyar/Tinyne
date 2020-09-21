3a5b095274c161dee83d9a885acfaa96d3f5f483
1khtiyar <ixty.now@bk.ru>
2020-09-21 21:05:17 +0400

    Dialog windows corrections: - Top most function; - Optimization; Style corrections. Bug fix.

--- TinyneProject/TinyneProject/ViewModels/MainWindowViewModel.cs
+++ TinyneProject/TinyneProject/ViewModels/MainWindowViewModel.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+
 using TinyneProject.Dialogs;
 using TinyneProject.Models;
 using TinyneProject.Services;
@@ -16,37 +18,50 @@ namespace TinyneProject.ViewModels
     /// <summary>
     /// ViewModel for <seealso cref="MainWindow"/>
     /// </summary>
-    internal class MainWindowViewModel:OnPropertyChangedClass
+    internal class MainWindowViewModel : OnPropertyChangedClass
     {
         private ObservableCollection<Note> notes;
         public ObservableCollection<Note> Notes
         {
             get => notes;
-            set 
-            { 
+            set
+            {
                 notes = value;
                 OnPropertyChanged();
             }
         }
 
 
-        public SolidColorBrush brush;
+        private SolidColorBrush onTopButtonForegroundBrush;
+        public SolidColorBrush OnTopButtonForegroundBrush
+        {
+            get => onTopButtonForegroundBrush;
+            set
+            {
+                onTopButtonForegroundBrush = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         private StorageIOService iOService;
+
 
         private StorageData currentStorageData;
         public StorageData currentData
         {
             get { return currentStorageData; }
-            set 
-            { 
+            set
+            {
                 currentStorageData = value;
                 OnPropertyChanged();
             }
         }
 
-        private Window window;
-
+        /// <summary>
+        /// Temp variable to have a reference of a main window
+        /// </summary>
+        private readonly Window window;
 
         #region Commands defining
 
@@ -69,25 +84,40 @@ namespace TinyneProject.ViewModels
             currentData = iOService.Load();
             this.Notes = currentData.Notes;
 
-            brush = (SolidColorBrush)Application.Current.FindResource("YellowBackgroundBrush");
+            OnTopButtonForegroundBrush = (SolidColorBrush)Application.Current.FindResource("WindowButtonForeBrush");
+
+            window.MouseLeave += Window_MouseLeave;
+            window.MouseEnter += Window_MouseEnter;
 
             #region Commands initializing
 
             CloseWindowCommand = new RelayCommand(CloseWindowClick);
-            MinimizeWindowCommand= new RelayCommand(MinimizeWindowClick);
-            TopMostWindowCommand= new RelayCommand(TopMostWindowClick);
-            LogoWindowCommand= new RelayCommand(LogoWindowClick);
+            MinimizeWindowCommand = new RelayCommand(MinimizeWindowClick);
+            TopMostWindowCommand = new RelayCommand(TopMostWindowClick);
+            LogoWindowCommand = new RelayCommand(LogoWindowClick);
 
-            AddNoteCommand= new RelayCommand(AddNoteClick);
-            DeleteNoteCommand= new ParameterizedRelayCommand(DeleteNoteClick);
-            EditNoteCommand= new ParameterizedRelayCommand(EditNoteClick);
+            AddNoteCommand = new RelayCommand(AddNoteClick);
+            DeleteNoteCommand = new ParameterizedRelayCommand(DeleteNoteClick);
+            EditNoteCommand = new ParameterizedRelayCommand(EditNoteClick);
 
             #endregion
         }
 
+        private void Window_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            window.Opacity = 1D;
+        }
+
+        private void Window_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (!window.Topmost) return;
+
+            window.Opacity = 0.2D;
+        }
+
         private void EditNoteClick(object note)
         {
-            EditNoteWindowDialog windowDialog = new EditNoteWindowDialog(note as Note,window);
+            EditNoteWindowDialog windowDialog = new EditNoteWindowDialog(note as Note, window);
 
             if (windowDialog.ShowDialog() == true)
             {
@@ -118,6 +148,7 @@ namespace TinyneProject.ViewModels
         private void LogoWindowClick()
         {
             AboutWindow aboutWindow = new AboutWindow();
+            aboutWindow.Owner = window;
 
             if (aboutWindow.ShowDialog() == true)
                 return;
@@ -126,6 +157,7 @@ namespace TinyneProject.ViewModels
         private void TopMostWindowClick()
         {
             window.Topmost = !window.Topmost;
+            OnTopButtonForegroundBrush = window.Topmost?(SolidColorBrush)Application.Current.FindResource("WhiteBrush7Opac"): (SolidColorBrush)Application.Current.FindResource("WindowButtonForeBrush");
         }
 
         private void MinimizeWindowClick()
--- TinyneProject/TinyneProject/Views/AboutWindow.xaml.cs
+++ TinyneProject/TinyneProject/Views/AboutWindow.xaml.cs
@@ -14,14 +14,13 @@ using System.Windows.Shapes;
 
 namespace TinyneProject.Views
 {
-    /// <summary>
-    /// Логика взаимодействия для AboutWindow.xaml
-    /// </summary>
     public partial class AboutWindow : Window
     {
         public AboutWindow()
         {
             InitializeComponent();
+
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
